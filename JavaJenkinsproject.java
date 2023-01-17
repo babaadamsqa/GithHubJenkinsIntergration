ac06eec730d6c3744665c0abb9d813b997120ed8
babaadamsqa <120124251+babaadamsqa@users.noreply.github.com>
2023-01-17 22:39:40 +0000

    JavaJenkinsproject.java added
    JavaJenkinsproject.java class added in the repository

---
+++ JavaJenkinsproject.java
@@ -0,0 +1,12 @@
+public class jenkinspractice {
+
+	public static void main(String[] args) {
+		for(int i=0; i<=10;i++)
+		{
+			System.out.println("Run Eclipse program using cmd");
+			System.out.println(i);
+		}
+
+	}
+
+}
